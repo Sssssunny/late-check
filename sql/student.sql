e9e131a016c9c80e0bb718457b22ab38064b4c33
Sssssunny <hhe330@naver.com>
2019-11-04 16:06:39 +0900

    DB에 학생 학번, 이름, 기본값 입력 (Insert)

---
+++ sql/student.sql
@@ -0,0 +1,74 @@
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21201', '김수아', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21202', '김영훈', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21203', '김우빈', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21204', '김현준', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21205', '문서준', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21206', '박수진', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21207', '박형진', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21208', '송도예', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21209', '신연주', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21210', '양소정', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21211', '이다빈', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21212', '이민희', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21213', '이서진', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21214', '이성민', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21215', '이소현', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21216', '이유빈', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21217', '이지한', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21218', '이지혜', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21219', '임효상', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21220', '정명철', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21221', '조건호', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21222', '조원석', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21223', '진상훈', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21224', '황수현', 0, 0, 0, 0, 500);
+
+INSERT INTO `late-check`.`student` (`student_id`, `student_name`, `late_850`, `late_900`, `late_month`, `now_money`, `next_money`)
+VALUES ('21225', '황순성', 0, 0, 0, 0, 500);
