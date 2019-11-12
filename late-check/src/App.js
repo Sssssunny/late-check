cc4865fbf7368f55236530084aae0f516d743a75
Sssssunny <hhe330@naver.com>
2019-11-12 11:02:08 +0900

    karen 스타일시트/JS 파일과 병합

--- late-check/src/App.js
+++ late-check/src/App.js
@@ -1,15 +1,53 @@
 import React from "react";
-import Main from './views/Main';
-// import lateCheck from './views/lateCheck';
-import Home from "./Home";
+import "./App.css";
+import "./Home.css";
+import { Route, Link } from "react-router-dom";
+import lateCheck from "./views/lateCheck";
+import Home from "./views/Home";
 import lateRead from "./views/lateRead";
-import './App.css';
-import './Home.css';
-import './views/Main.css';
 
-
-function App() {
-  return <Main/>;
-}
+const App = () => {
+  return (
+    <div>
+      <div class="layer" id="wapper">
+        <span class="content">
+          {/* 헤더 시작 */}
+          <header>
+            <p>지각 체크 프로그램이지롱!</p>
+          </header>
+          {/* 네비게이션 */}
+          <nav>
+            <ul>
+              <li>
+                <Link className="link" to="/lateCheck">
+                  체크
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/">
+                  소개
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/lateRead">
+                  조회
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* 콘텐츠 부분 */}
+          <section>
+            <article>
+              <Route path="/" component={Home} exact={true} />
+              <Route path="/lateCheck" component={lateCheck} />
+              <Route path="/lateRead" component={lateRead} />
+            </article>
+          </section>
+        </span>
+        <span class="blank"></span>
+      </div>
+    </div>
+  );
+};
 
 export default App;
